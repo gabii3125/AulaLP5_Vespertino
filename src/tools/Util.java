e9d5b67c2a88110cf3ea380c1df5a5205f1c4643
gabii3125 <gabriela.dolacio@estudante.ifms.edu.br>
2025-09-09 23:17:45 -0300

    fiz os métodos de conversão de tipos no util.

--- src/tools/Util.java
+++ src/tools/Util.java
@@ -4,6 +4,9 @@
  */
 package tools;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
@@ -14,37 +17,67 @@ import javax.swing.JTextField;
  * @author u06075330135
  */
 public class Util {
-    public  static void habilitar(boolean  valor, JComponent ... componentes){
+
+    public static void habilitar(boolean valor, JComponent... componentes) {
         for (int i = 0; i < componentes.length; i++) {
-             componentes[i].setEnabled(valor);
-            
+            componentes[i].setEnabled(valor);
+
         }
     }
-        public static void Limpar(JComponent... components){
-            for (int i = 0; i < components.length; i++) {
-                 if (components[i] instanceof  JTextField) {
-                ((JTextField)components[i]).setText("");
-                
+
+    public static void Limpar(JComponent... components) {
+        for (int i = 0; i < components.length; i++) {
+            if (components[i] instanceof JTextField) {
+                ((JTextField) components[i]).setText("");
+
             }
-                 if (components[i] instanceof  JComboBox) {
-                ((JComboBox)components[i]).setSelectedIndex(-1);
-                
-            }     
-                
+            if (components[i] instanceof JComboBox) {
+                ((JComboBox) components[i]).setSelectedIndex(-1);
+
             }
+
         }
-        public static void mensagem(String cad) {
-            JOptionPane.showMessageDialog(null, cad);
-        }
-        public static void pergunta(String cad){
-            JOptionPane.showConfirmDialog(null, cad);
-        
     }
-        public static int strToInt(String num) {
+
+    public static void mensagem(String cad) {
+        JOptionPane.showMessageDialog(null, cad);
+    }
+
+    public static void pergunta(String cad) {
+        JOptionPane.showConfirmDialog(null, cad);   
+
+    }
+
+    public static int strToInt(String num) {
         return Integer.valueOf(num);
     }
-        public static String intToStr(int num){
-            return String.valueOf(num);
+
+    public static String intToStr(int num) {
+        return String.valueOf(num);
+    }
+
+    public static double strToDouble(String num) {
+         return Double.parseDouble(num);
+    }
+
+    public static String doubleTostr(double num) {
+       return String.valueOf(num);
+       
+    }
+
+   public static Date strToData(String num) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            return sdf.parse(num);
+        } catch (ParseException e) {
+            mensagem("Erro ao converter data: " + e.getMessage());
+            return null;
         }
-    
+    }
+
+    public static String dataToStr(Date num) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(num);
+    }
+
 }
